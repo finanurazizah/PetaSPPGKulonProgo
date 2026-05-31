35485f8f37058c3de08b2825956a366ea1af7226
finanurazizah <finanurazizahlintangsari@mail.ugm.ac.id>
2026-05-31 16:11:34 +0700

    Add files via upload

---
+++ lembar 3 - OpenLayers/styles/S_GirimulyoJatimulyo_13_style.js
@@ -0,0 +1,60 @@
+var size = 0;
+var placement = 'point';
+function categories_S_GirimulyoJatimulyo_13(feature, value, size, resolution, labelText,
+    labelFont, labelFill, bufferColor, bufferWidth,
+    placement) {
+    var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+    switch (valueStr) {
+        case 'Dekat':
+            return [new ol.style.Style({
+                stroke: new ol.style.Stroke({ color: 'rgba(102,194,165,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9 }),
+                text: createTextStyle(feature, resolution, labelText, labelFont,
+                    labelFill, placement, bufferColor,
+                    bufferWidth)
+            })];
+            break;
+        case 'Sedang':
+            return [new ol.style.Style({
+                stroke: new ol.style.Stroke({ color: 'rgba(255,165,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9 }),
+                text: createTextStyle(feature, resolution, labelText, labelFont,
+                    labelFill, placement, bufferColor,
+                    bufferWidth)
+            })];
+            break;
+        case 'Jauh':
+            return [new ol.style.Style({
+                stroke: new ol.style.Stroke({ color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9 }),
+                text: createTextStyle(feature, resolution, labelText, labelFont,
+                    labelFill, placement, bufferColor,
+                    bufferWidth)
+            })];
+            break;
+    }
+};
+
+var style_S_GirimulyoJatimulyo_13 = function (feature, resolution) {
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+
+    var labelText = "";
+    var value = feature.get("k_jarak");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+
+    var style = categories_S_GirimulyoJatimulyo_13(feature, value, size, resolution, labelText,
+        labelFont, labelFill, bufferColor,
+        bufferWidth, placement);
+
+    return style;
+};
